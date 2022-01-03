e9e58b41e08e66dbbddf2f5886adda89aa4d520e
Junho Park <dwarfthema@gmail.com>
2022-01-03 15:41:42 +0900

    ~3.3 install fin

---
+++ server.js
@@ -0,0 +1,24 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hiiiiii",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🤖 Server is Ready at ${url}`);
+});
