10ae8f022c0d3ed89e9abdc0efc31d55439a3b01
Junho Park <dwarfthema@gmail.com>
2022-01-04 16:45:28 +0900

    ~3.5 Prisma migration / postgres / postico

--- server.js
+++ server.js
@@ -2,14 +2,35 @@ import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
 const typeDefs = gql`
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hiiiiii",
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2022 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
