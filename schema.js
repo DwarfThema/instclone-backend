7390662ee9c20a46b3ac7bc4d660e64bb8144ba1
Junho Park <dwarfthema@gmail.com>
2022-01-05 23:23:48 +0900

    ~3.10 Architecture (divide & conquer)

---
+++ schema.js
@@ -0,0 +1,14 @@
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const typeDefs = mergeTypeDefs(loadedTypes);
+
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries,mutations}.js`
+);
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+export default schema;
--- server.js
+++ server.js
@@ -1,58 +1,9 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import { PrismaClient } from "@prisma/client";
-
-const client = new PrismaClient();
-
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int!): Movie
-    updataMovie(id: Int!, year: Int!): Movie
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
-  },
-  Mutation: {
-    createMovie: (_, { title, year, genre }) =>
-      client.movie.create({
-        data: {
-          title,
-          year,
-          genre,
-        },
-      }),
-    deleteMovie: (_, { id }) =>
-      client.movie.delete({
-        where: { id },
-      }),
-    updataMovie: (_, { id, year }) =>
-      client.movie.update({
-        where: { id },
-        data: { year },
-      }),
-  },
-};
+import schema from "./schema";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
