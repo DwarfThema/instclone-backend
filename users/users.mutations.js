a3939cd5308ce0af90414ebfde0a92eef702aa0f
Junho Park <dwarfthema@gmail.com>
2022-01-06 21:53:16 +0900

    ~4.3 seeProfile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -7,31 +7,38 @@ export default {
       _,
       { firstName, lastName, userName, email, password }
     ) => {
-      // check if userName or email are already on DB.
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              userName,
-            },
-            {
-              email,
-            },
-          ],
-        },
-      });
-      const uglyPassword = await bcrypt.hash(password, 10);
-      return client.user.create({
-        data: {
-          userName,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword,
-        },
-      });
-      // hash password , hashing makes the passwork looks ugly
-      // save and return the user
+      try {
+        // check if userName or email are already on DB.
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                userName,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/password is already taken.");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            userName,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+        // hash password , hashing makes the passwork looks ugly
+        // save and return the user
+      } catch (e) {
+        return e;
+      }
     },
   },
 };
