56c69fce0e38b57304a9fde6e9ed184e1ee008e3
Junho Park <dwarfthema@gmail.com>
2022-01-11 00:34:03 +0900

    ~4.12 protectedResolver : fn(fn)

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import schema from "./schema";
-import { getUser } from "./users/users.utils";
+import { getUser, protectResolver } from "./users/users.utils";
 
 const server = new ApolloServer({
   schema,
@@ -14,6 +14,9 @@ const server = new ApolloServer({
 
       // token: req.headers.token,
       //html 에서 보내는 headers 정보를 vsc에서 사용 할 수 있다. 우리는 token 정보를 이용하자.
+
+      protectResolver,
+      // protectResolver 는 user가 있는지 없는지 확인한 뒤, 없으면 error message를 throw 해주는 유틸이다.
     };
   },
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,47 +1,48 @@
 import client from "../../client";
-import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { protectedResolver } from "../users.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, userName, email, password: newPassword },
-      { loggedInUser }
-    ) =>
-      //context는 graphql의 4번째 arg. 모든 resolver에서 접근가능한 정보를 넣을 수 있는 object : 여기 말고도 createAccout나 login 에서도 접근 가능하다는것
-      //Constext의 정의는 appoloserver가 있는 server 에서 정의할 수 있음
-      //위에서는 {token}의 obejct 정보를 갖고와서 여기서 사용된다.
-      //이후에 loggedInUser로 변경되어서 사용된다.
-      {
-        console.log(loggedInUser);
-        let uglyPassword = null;
-        if (newPassword) {
-          uglyPassword = await bcrypt.hash(newPassword, 10);
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, userName, email, password: newPassword },
+        { loggedInUser, protectResolver }
+      ) =>
+        //context는 graphql resolver의 4번째 arg. 모든 resolver에서 접근가능한 정보를 넣을 수 있는 object : 여기 말고도 createAccout나 login 에서도 접근 가능하다는것
+        //Constext의 정의는 appoloserver가 있는 server 에서 정의할 수 있음
+        //위에서는 {token}의 obejct 정보를 갖고와서 여기서 사용된다.
+        //이후에 loggedInUser로 변경되어서 사용된다.
+        {
+          let uglyPassword = null;
+          if (newPassword) {
+            uglyPassword = await bcrypt.hash(newPassword, 10);
+          }
+          const updatedUser = await client.user.update({
+            where: {
+              id: loggedInUser.id,
+            },
+            data: {
+              firstName,
+              lastName,
+              userName,
+              email,
+              ...(uglyPassword && { password: uglyPassword }),
+              //uglyPassword가 true 면 Password = uglyPassword 한다. ...은 중괄호를 풀어주는 역할.
+            },
+          });
+          if (updatedUser.id) {
+            return {
+              ok: true,
+            };
+          } else {
+            return {
+              ok: false,
+              error: "Could not update profile",
+            };
+          }
         }
-        const updatedUser = await client.user.update({
-          where: {
-            id: loggedInUser.id,
-          },
-          data: {
-            firstName,
-            lastName,
-            userName,
-            email,
-            ...(uglyPassword && { password: uglyPassword }),
-            //uglyPassword가 true 면 Password = uglyPassword 한다. ...은 중괄호를 풀어주는 역할.
-          },
-        });
-        if (updatedUser.id) {
-          return {
-            ok: true,
-          };
-        } else {
-          return {
-            ok: false,
-            error: "Could not update profile",
-          };
-        }
-      },
+    ),
   },
 };
