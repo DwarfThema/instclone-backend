c9df7f4c49c5fd0ad03be33f331925b5d66d3fcf
Junho Park <dwarfthema@gmail.com>
2022-01-15 13:13:46 +0900

    ~6.4 upload Photo connectOrCreate

--- src/photos/uploadPhoto/uploadPhoto.resolvers.ts
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.ts
@@ -1,13 +1,35 @@
+import client from "../../client";
 import { Resolver, Resolvers } from "../../types";
 import { protectedResolver } from "../../users/users.utils";
 
 const resolverFn: Resolver = async (_, { file, caption }, { loggedInUser }) => {
+  let hashtagObj = [];
   if (caption) {
     /// parse caption
-    // get or create Hashtags
     const hashtags = caption.match(/#[\w]+/g);
+    hashtagObj = hashtags.map((hashtag) => ({
+      where: { hashtag },
+      create: { hashtag },
+    }));
     console.log(hashtags);
   }
+  // get or create Hashtags
+  return client.photo.create({
+    data: {
+      file,
+      caption,
+      user: {
+        connect: {
+          id: loggedInUser.id,
+        },
+      },
+      ...(hashtagObj.length > 0 && {
+        hashtag: {
+          connectOrCreate: hashtagObj,
+        },
+      }),
+    },
+  });
   // save the photo Whit the parsed hashtags
   // add the photo to the hashtags
 };
