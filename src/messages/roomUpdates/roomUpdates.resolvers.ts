95285c1945d8fa0ab967ae93eb3a8172bea03585
Junho Park <dwarfthema@gmail.com>
2022-03-18 12:34:02 +0900

    ~7.10 withFilter fix

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -1,16 +1,33 @@
 import { withFilter } from "graphql-subscriptions";
+import client from "../../client";
 import { NEW__MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
+import { Resolver } from "../../types";
+
+const resolverFn: Resolver = async (root, args, context, info) => {
+  const room = await client.room.findUnique({
+    where: {
+      id: args.id,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (!room) {
+    throw new Error("You shall not see this.");
+  }
+  return withFilter(
+    () => pubsub.asyncIterator(NEW__MESSAGE),
+    ({ roomUpdates }, { id }) => {
+      return roomUpdates.roomId === id;
+    }
+  )(root, args, context, info);
+};
 
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: withFilter(
-        () => pubsub.asyncIterator(NEW__MESSAGE),
-        ({ roomUpdates }, { id }) => {
-          return roomUpdates.roomId === id;
-        }
-      ),
+      subscribe: resolverFn,
     },
   },
 };
