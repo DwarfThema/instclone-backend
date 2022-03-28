f575ce55b5c63358985f3c99bc50ddccadba03db
Junho Park <dwarfthema@gmail.com>
2022-03-28 15:34:21 +0900

    me fix

--- src/users/me/me.resolvers.ts
+++ src/users/me/me.resolvers.ts
@@ -1,18 +1,17 @@
 import client from "../../client";
 import { Resolver, Resolvers } from "../../types";
-import { protetedResolver } from "../users.utils";
+import { protectedResolver } from "../users.utils";
 
-const resolverFn: Resolver = (_, __, { loggedInUser }) => {
+const resolverFn: Resolver = (_, __, { loggedInUser }) =>
   client.user.findUnique({
     where: {
       id: loggedInUser.id,
     },
   });
-};
 
 const resolvers: Resolvers = {
   Query: {
-    me: protetedResolver(resolverFn),
+    me: protectedResolver(resolverFn),
   },
 };
 
