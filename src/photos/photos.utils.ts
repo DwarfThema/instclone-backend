bb7b3eef69201f29406abf7ac38957c12576f9d7
Junho Park <dwarfthema@gmail.com>
2022-03-29 17:18:52 +0900

    photoResolvers_commentNumber comments

--- src/photos/photos.utils.ts
+++ src/photos/photos.utils.ts
@@ -1,5 +1,5 @@
 export const processHashtags = (caption) => {
-  const hashtags = caption.match(/#[\w]+/g) || [];
+  const hashtags = caption.match(/#[\d|A-Z|a-z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+/g) || [];
   return hashtags.map((hashtag) => ({
     where: { hashtag },
     create: { hashtag },
